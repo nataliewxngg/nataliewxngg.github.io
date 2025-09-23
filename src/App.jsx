335762bb293c7507ebfb2770923f834d8668205c
Natalie Wong <wong.lt.natalie@gmail.com>
2025-09-22 20:29:23 -0400

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import AboutMe from "./Pages/AboutMe"
+import Projects from "./Pages/Projects"
+
 export default function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about-me" element={<AboutMe />}/>
+        <Route path="/projects" element={<Projects />}/>
+      </Routes>
+    </Router>
   )
 }
---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+    return (
+        <h1>Project Page</h1>
+    )
+}
