03fd64d121c51d82f0f7515e805cc9af637f2ec3
Natalie Wong <wong.lt.natalie@gmail.com>
2025-09-22 22:05:11 -0400

    Setup theme colors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,6 @@
 export default function Home(props) {
     return (
         <>
-            <header>
-                <button onClick={props.toggleDarkMode}>
-                    Toggle Dark Mode
-                </button>
-            </header>
-            <main className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark`}>
-                <h1>Home Page</h1>
-            </main>
         </>
     )
 }
