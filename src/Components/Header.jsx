a20db4cfba88b03536c4ddb695f5e38c048446b3
nataliewxngg <wong.lt.natalie@gmail.com>
2025-09-23 08:38:36 -0400

    Add name on homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,8 @@ import sunSVG from '../assets/sun.svg'
 
 export default function Header(props) {
     return (
-        <header className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark`}>
+        <header className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark
+         pt-30 flex justify-between items-center`}>
             <button onClick={props.toggleDarkMode}>
                 <img src={props.isDarkMode ? sunSVG : moonSVG} 
                 alt={props.isDarkMode ? "Sun" : "Moon"} 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,9 @@ export default function Home(props) {
     return (
         <>
             <Header isDarkMode={props.isDarkMode} toggleDarkMode={props.toggleDarkMode} />
+            <section className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark py-10`}>
+                <h1 className="text-[10rem] font-extrabold leading-[0.8]">Natalie <br></br><span>Wong</span></h1>
+            </section>
         </>
     )
 }
