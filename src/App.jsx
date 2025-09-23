cb2f27ff78a3f4ee32b5e4995c6320304ad7699f
Natalie Wong <wong.lt.natalie@gmail.com>
2025-09-22 21:13:10 -0400

    Setup theme toggling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
 import Home from "./Pages/Home"
 import AboutMe from "./Pages/AboutMe"
 import Projects from "./Pages/Projects"
 
 export default function App() {
+
+  // Dark mode state
+  const [isDarkMode, setIsDarkMode] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches);
+  function toggleDarkMode() {
+    setIsDarkMode(isDarkMode => !isDarkMode);
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about-me" element={<AboutMe />}/>
-        <Route path="/projects" element={<Projects />}/>
+        {/* Home Page */}
+        <Route path="/" element={<Home isDarkMode={isDarkMode} 
+        toggleDarkMode={toggleDarkMode} />}/>
+
+        {/* About Me Page */} 
+        <Route path="/about-me" element={<AboutMe isDarkMode={isDarkMode} 
+        toggleDarkMode={toggleDarkMode} />}/>
+
+        {/* Projects Page */}
+        <Route path="/projects" element={<Projects isDarkMode={isDarkMode} 
+        toggleDarkMode={toggleDarkMode} />}/>
       </Routes>
     </Router>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,14 @@
-export default function Home() {
+export default function Home(props) {
     return (
-        <h1>Home Page</h1>
+        <>
+            <header>
+                <button onClick={props.toggleDarkMode}>
+                    Toggle Dark Mode
+                </button>
+            </header>
+            <main className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark`}>
+                <h1>Home Page</h1>
+            </main>
+        </>
     )
 }
