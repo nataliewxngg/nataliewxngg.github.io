9d98819ffdee3ce92c4474beb9b07dbf8e939c10
nataliewxngg <wong.lt.natalie@gmail.com>
2025-09-24 08:08:56 -0400

    Add description in homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,13 +3,13 @@ import sunSVG from '../assets/sun.svg'
 
 export default function Header(props) {
     return (
-        <header className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark
-         pt-30 flex justify-between items-center`}>
-            <button onClick={props.toggleDarkMode}>
+        <header className="flex justify-between items-center">
+            <button onClick={props.toggleDarkMode} className="bg-bg dark:bg-bg-dark">
                 <img src={props.isDarkMode ? sunSVG : moonSVG} 
                 alt={props.isDarkMode ? "Sun" : "Moon"} 
                 className="w-10 h-10 cursor-pointer" />
             </button>
+            <button className="bg-button-bg dark:bg-button-bg-dark">contact</button>
         </header>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,14 @@ import Header from "../Components/Header"
 
 export default function Home(props) {
     return (
-        <>
-            <Header isDarkMode={props.isDarkMode} toggleDarkMode={props.toggleDarkMode} />
+        <main className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark 
+            min-h-screen py-20 px-130
+            flex flex-col gap-20`}>
+            <Header toggleDarkMode={props.toggleDarkMode} isDarkMode={props.isDarkMode} />
             <section className={`${props.isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark py-10`}>
                 <h1 className="text-[10rem] font-extrabold leading-[0.8]">Natalie <br></br><span>Wong</span></h1>
+                <p className="mt-10 text-3xl font-medium">Aspiring <span className="text-secondary dark:text-secondary-dark">full-stack developer</span> with a passion for creating web applications and discovering new technologies.</p>
             </section>
-        </>
+        </main>
     )
 }
