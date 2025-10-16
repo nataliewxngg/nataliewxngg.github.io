84d94b48b04b2e0f25e3bc99d770ee761f97016a
nataliewxngg <wong.lt.natalie@gmail.com>
2025-10-15 22:10:33 -0400

    Setup for New Wireframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom"
-import { useState, useEffect } from "react"
-import Home from "./Pages/Home"
-import AboutMe from "./Pages/AboutMe"
-import Projects from "./Pages/Projects"
+import { useState } from 'react';
+import Header from './components/Header';
 
 export default function App() {
 
@@ -13,20 +10,18 @@ export default function App() {
   }
 
   return (
-    <Router>
-      <Routes>
-        {/* Home Page */}
-        <Route path="/" element={<Home isDarkMode={isDarkMode} 
-        toggleDarkMode={toggleDarkMode} />}/>
-
-        {/* About Me Page */} 
-        <Route path="/about-me" element={<AboutMe isDarkMode={isDarkMode} 
-        toggleDarkMode={toggleDarkMode} />}/>
-
-        {/* Projects Page */}
-        <Route path="/projects" element={<Projects isDarkMode={isDarkMode} 
-        toggleDarkMode={toggleDarkMode} />}/>
-      </Routes>
-    </Router>
+    <main
+      className={`${isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark 
+                  min-h-screen py-20 px-130 flex flex-col gap-20`}>
+      
+      {/* Header */}
+      <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+    
+      <section className={`${isDarkMode ? "dark" : ""} bg-bg dark:bg-bg-dark text-primary dark:text-primary-dark py-10`}>
+          <h1 className="text-[10rem] leading-[0.8] font-bold">Natalie <br></br><span>Wong</span></h1>
+          <p className="mt-10 text-3xl font-extralight">Aspiring <span className="text-secondary dark:text-secondary-dark">full-stack developer</span> with a passion for creating web applications and discovering new technologies.</p>
+      </section>
+      
+    </main>
   )
 }
--- src/Pages/AboutMe.jsx
+++
@@ -1,5 +0,0 @@
-export default function AboutMe() {
-    return (
-        <h1>About Me Page</h1>
-    )
-}
