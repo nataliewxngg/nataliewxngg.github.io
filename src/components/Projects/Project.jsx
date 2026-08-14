d23af98e33c5baf303c4d3feb44bab15aa14e52c
Natalie Wong <wong.lt.natalie@gmail.com>
2026-08-14 16:08:24 -0400

    Add AccordianGallery for projects showcase

--- src/components/Projects/Project.jsx
+++
@@ -1,8 +0,0 @@
-function Project(props) {
-    return <div className='text-center h-full'>
-        {/* TODO: Update the alt for the image */}
-        <img src={props.img} alt='' className='w-full h-full aspect-square object-cover rounded-xl' />
-    </div>
-}
-
-export default Project;
